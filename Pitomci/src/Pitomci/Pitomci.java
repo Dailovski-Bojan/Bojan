9b0558d65147272bdf428a26f02efdc2e507ca02
Dailovski-Bojan <bojan04dailovski@gmail.com>
2025-10-26 18:01:14 +0100

    +

---
+++ Pitomci/src/Pitomci/Pitomci.java
@@ -0,0 +1,13 @@
+
+//koristime paket narecen pitomci
+package Pitomci; 
+//deklalirame klasa narecena Pitomci
+public class Pitomci { 
+//ova e glavniot main metod sto se koristi po default
+	public static void main(String[] args) { //
+//so ovaa linija go pecatime tekstot
+		System.out.println("Nie sme pitomci od 29ta klasa");
+		
+	}
+
+}
