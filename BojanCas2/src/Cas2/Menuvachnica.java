19ac507ab080a7d90d08d183962e8988076eac92
Dailovski-Bojan <bojan04dailovski@gmail.com>
2025-10-26 17:52:14 +0100

    0

---
+++ BojanCas2/src/Cas2/Menuvachnica.java
@@ -0,0 +1,14 @@
+package Cas2;
+
+	public class Menuvachnica {
+	public static void main(String[]args) {
+			
+			double euro =3;
+			double denari = euro * 61.5;
+			System.out.println("Denari: "+denari);
+		
+	}
+	
+
+}
+//presmetaj ja cenata n
